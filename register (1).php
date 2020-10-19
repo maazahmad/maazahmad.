a1fd04cacb96a823d451d58bbd61bf242ef3780f
Maaz Ahmad <maazahmad36@gmail.com>
2020-10-19 22:51:30 +0500

    update
    update

---
+++ register (1).php
@@ -0,0 +1,72 @@
+<?php
+
+$field_first_name = $_POST['names'];
+
+$field_email = $_POST['email'];
+
+$field_phone = $_POST['phone'];
+
+$mail_to = 'contact@jeepramtrucks.com';
+
+$subject = 'Message from a site visitor '.$field_first_name;
+
+$body_message = 'From: '.$field_first_name."\n";
+
+$body_message .= 'E-mail: '.$field_email."\n";
+
+$body_message .= 'Message: '.$field_phone;
+
+$headers = 'From: '.$field_email."\r\n";
+
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+$email = $field_email;
+$list_id = 'ef21bbdcac';
+$api_key = '0373fca748081cda9312ae6dfdf18767-us4';
+ 
+$data_center = substr($api_key,strpos($api_key,'-')+1);
+ 
+$url = 'https://'. $data_center .'.api.mailchimp.com/3.0/lists/'. $list_id .'/members';
+ 
+$json = json_encode([
+    'email_address' => $email,
+    'status'        => 'subscribed', //pass 'subscribed' or 'pending'
+]);
+ 
+try {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $api_key);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    $result = curl_exec($ch);
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+ 
+    if (200 == $status_code) {
+        echo "The user added successfully to the MailChimp.";
+    }
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly.');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		//alert('Message failed. Please, send an email to gordon@template-help.com');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+?>
